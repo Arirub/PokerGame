3d3bd69deeca5d3d0253f7a33321197470bb4afd
ariane <ariane@arianes-mbp>
2017-02-04 14:53:28 +0100

    Add new Enum.

---
+++ pokerModel/src/main/java/de/flare/poker/game/model/enums/Category.java
@@ -0,0 +1,29 @@
+package de.flare.poker.game.model.enums;
+
+public enum Category {
+
+	HIGH_CARD(1),
+	PAIR(2),
+	TWO_PAIRS(3),
+	THREE_OF_A_KIND(4),
+	STRAIGHT(5),
+	FLUSH(6),
+	FULL_HOUSE(7),
+	POKER(8),
+	STRAIGHT_FLUSH(9);
+	
+	private int category;
+
+	private Category(int category) {
+		this.category = category;
+	}
+
+	public int getCategory() {
+		return category;
+	}
+
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	
+}
